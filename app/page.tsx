1a4e21ac8debbfc32a89eaa65a3636b29c7fafd8
Alex Olson <alexko@bu.edu>
2025-12-02 19:04:23 -0500

    placeholder for future components

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ export default function Home() {
       // Main Page Components Here (zach / alex)
       <div className="flex w-3/4 m-auto flex-col align-items-center bg-green-100 min-h-screen">
         <Header/>
+          <p className="text-black text-2xl"> rest of components/content </p>
         <Footer/>
       </div>
   );
